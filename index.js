7d8aa5dbb9c56d3af4460098f61728c585125e94
Isaac Sokari <isaacsokari@gmail.com>
2021-01-17 13:46:26 +0100

    added if else block for redirection

--- index.js
+++ index.js
@@ -13,8 +13,11 @@ app.use(cors());
 // redirect to https on production
 if (process.env.NODE_ENV === 'production') {
   app.get('*', (req, res, next) => {
-    if (req.secure) next();
-    res.redirect(`https://${req.headers.host}${req.url}`);
+    if (req.secure) {
+      next();
+    } else {
+      res.redirect(`https://${req.headers.host}${req.url}`);
+    }
   });
 }
 
