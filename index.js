f237c5b44134696ced8118ce5837fa30ca455dff
Isaac Sokari <isaacsokari@gmail.com>
2021-01-17 14:46:33 +0100

    feat: added azure check to redirection middleware and moved function to utils file

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const cors = require('cors');
 const path = require('path');
 
-const { getDate } = require('./utils');
+const { getDate, redirectToHttps } = require('./utils');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,27 +12,7 @@ app.use(cors());
 
 // redirect to https on production
 if (process.env.NODE_ENV === 'production') {
-  app.use((req, res, next) => {
-    let isSecure = req.secure;
-
-    // set isSecure for heroku
-    if (!isSecure && req.headers['x-forwarded-proto']) {
-      isSecure = req.headers['x-forwarded-proto'].substr(0, 5) === 'https';
-    }
-
-    if (isSecure) {
-      next();
-    } else {
-      // for only get requests
-      if (req.method === 'GET' || req.method === 'HEAD') {
-        res.redirect(301, `https://${req.headers.host}${req.url}`);
-      } else {
-        res
-          .status(403)
-          .send('Please use https when submitting to this server.');
-      }
-    }
-  });
+  app.use(redirectToHttps);
 }
 
 app.get('/', (req, res) => {
--- utils.js
+++ utils.js
@@ -7,4 +7,29 @@ const getDate = () => {
   return `${year}-${month < 10 ? `0${month}` : month}-${day}`;
 };
 
-module.exports = { getDate };
+const redirectToHttps = (req, res, next) => {
+  let isSecure = req.secure;
+
+  // set isSecure for heroku
+  if (!isSecure && req.headers['x-forwarded-proto']) {
+    isSecure = req.headers['x-forwarded-proto'].substr(0, 5) === 'https';
+  }
+
+  // for azure
+  if (!isSecure && req.headers['x-arr-ssl']) {
+    isSecure = true;
+  }
+
+  if (isSecure) {
+    next();
+  } else {
+    // for only get requests
+    if (req.method === 'GET' || req.method === 'HEAD') {
+      res.redirect(301, `https://${req.headers.host}${req.url}`);
+    } else {
+      res.status(403).send('Please use https when submitting to this server.');
+    }
+  }
+};
+
+module.exports = { getDate, redirectToHttps };
